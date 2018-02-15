bf5912c542fdc7eae5d0cb2eba9c78ba68039e45
TimNysetvold <tim.nysetvold@gmail.com>
2018-02-14 18:49:09 -0700

    Revised revenue structures for camera and GPS. Added NPV and depreciation functions

---
+++ NPV.m
@@ -0,0 +1,10 @@
+function [ present ] = NPV(revenue_matrix)
+%this function returns the net present value of a cost structure.
+discount_rate=.08;
+present=0;
+for i=1:length(revenue_matrix)
+    present=present+revenue_matrix(i)/(1+discount_rate)^i;
+end
+
+end
+
--- RocketCosts.m
+++ RocketCosts.m
@@ -2,11 +2,11 @@ function [cost] = RocketCosts(total_weight)
 %This function performs the rocket equation analysis to determine how much
 %fuel is needed to get to orbit. It returns the total cost of the fuel.
 
-%This is currently a placeholder.
+%Documentation to follow.
 
     liq_h_cost = 4;          %cost for liquid hydrogen fuel ($/kg)
     liq_oxy_cost = 0.20;     %cost for liquid oxygen fuel ($/kg)
-    h_oxy_ratio = 5.5;       %ratio for 
+    h_oxy_ratio = 5.5;       %Hydrogen/oxygen ratio.
     i_sp = 450;              %specific impulse of fuel
     delta_v = 3070;          %necessary orbital velocity (m/s)
     mf = 46697+total_weight; %dry mass of Atlas V rocket and satellite(kg)
@@ -20,7 +20,7 @@ function [cost] = RocketCosts(total_weight)
     mass_liq_oxy = mass_propellant/(h_oxy_ratio+1);
     mass_liq_h = h_oxy_ratio*mass_liq_oxy;
     
-    cost=liq_h_cost*mass_liq_h + liq_oxy_cost*mass_liq_oxy;  %placeholder
+    cost=liq_h_cost*mass_liq_h + liq_oxy_cost*mass_liq_oxy;  
 
 end
 
